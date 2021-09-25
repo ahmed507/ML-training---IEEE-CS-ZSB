07ba4e0bd2fb7c774d67789ca478c0a79d377094
ahmed507 <ahmedhamada507@gmail.com>
2021-09-25 19:11:25 +0200

    Update gradient_descent.py

--- Task2-Gradient-Descent-Linear-Regression/gradient_descent.py
+++ Task2-Gradient-Descent-Linear-Regression/gradient_descent.py
@@ -14,15 +14,17 @@ m = len(x)
 alpha = 0.005
 theta = [1,1]
 h = [None]*m
+y_res = []
 for i in range(m):
     h[i] = theta[0] + theta[1] * x[i]
     theta[0] = theta[0] - ((alpha / m) * (h[i] - y[i]))
     theta[1] = theta[1] - (((alpha / m) * (h[i] - y[i]))*x[i])
 for j in range(len(x)):
     hyp = theta[0] + theta[1] * x[j]
-    plt.plot(x[j],hyp,"x")
+    y_res.append(hyp)
 # print(theta)
 plt.plot(x, y, "o")
+plt.plot(x,y_res)
 plt.xlabel("X")
 plt.ylabel("Y")
 plt.show()
